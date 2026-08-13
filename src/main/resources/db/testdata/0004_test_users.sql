9b971815e53812e6e1fc772805e52c2896df7020
Matt <matek851@gmail.com>
2026-08-13 11:35:03 +0200

    add test users

---
+++ src/main/resources/db/testdata/0004_test_users.sql
@@ -0,0 +1,6 @@
+insert into
+    users (email, password, nick, activ, activ_key)
+values
+    ('admin@admin.pl', '{noop}admin', 'admin', true, 'sdf435345jn'),   -- 1
+    ('user@user.pl', '{noop}user', 'user', true, 'sfedfg87'); -- 2
+
---
+++ src/main/resources/db/testdata/0007_user_role.sql
@@ -0,0 +1,6 @@
+insert into
+    user_role (name, description)
+values
+    ('ADMIN', 'pełne uprawnienia'),   -- 1
+    ('USER', 'podstawowe uprawnienia, możliwość oddawania głosów, komentowania, polubienia'),  -- 2
+    ('BLOCKED', 'zablokowany użytkownik');
